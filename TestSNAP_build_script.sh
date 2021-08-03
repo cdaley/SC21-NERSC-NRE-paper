e3dfcc351c71eabb58e6c5990437e2b68606cfe0
rgayatri <rgayatri@lbl.gov>
2021-08-02 18:58:44 -0700

    Edit TestSNAP build script to rely on Kokkos install from the Kokkos build script.

--- TestSNAP_build_script.sh
+++ TestSNAP_build_script.sh
@@ -14,92 +14,59 @@ export OMP_NUM_THREADS=1
 export OMP_PLACES=threads
 export OMP_PROC_BIND=spread
 
-#ARCH="VOLTA70"
-ARCH="AMPERE80"
+RUN="srun -n 1 -c 16 --cpu-bind=cores"
 
-if [[ ${ARCH} == "VOLTA70" ]] 
-then
-    RUN="srun -n 1 -c 10 --cpu-bind=cores"
-else
-    RUN="srun -n 1 -c 16 --cpu-bind=cores"
+HOME=$(pwd)
+
+if [ ! -d kokkos ]; then
+    echo "Kokkos not found. Please run the Kokkos script first."
+    exit 1
 fi
 
-BUILD_KOKKOS=1
 KOKKOS_PATH=$(pwd)/kokkos
+cd ${KOKKOS_PATH}
 
-if [[ $BUILD_KOKKOS -eq 1 ]]; then
-    if [ ! -d kokkos ]; then
-        git clone --single-branch --branch develop https://github.com/kokkos/kokkos.git
-    fi 
-    cd kokkos
-    if [ -d build_cuda_nvhpc_${ARCH} ]; then
-        rm -rf build_cuda_nvhpc
-    fi
-    mkdir build_cuda_nvhpc_${ARCH} && cd build_cuda_nvhpc_${ARCH}
-    cmake -D CMAKE_BUILD_TYPE=Release \
-	  -D CMAKE_INSTALL_PREFIX=${KOKKOS_PATH}/install_cuda_nvhpc \
-	  -D CMAKE_CXX_COMPILER=${KOKKOS_PATH}/bin/nvcc_wrapper \
-      -D CMAKE_C_COMPILER= gcc \
-	  -D CMAKE_CXX_STANDARD=17 \
-	  -D Kokkos_ARCH_${ARCH}=ON \
-	  -D Kokkos_ENABLE_CUDA=ON \
-	  -D Kokkos_ENABLE_CUDA_LAMBDA=ON \
-	  ..
-    make -j8
-    make install
-cd ..
-
-    if [ ! -d build_cuda_nvhpc_${ARCH} ]; then
-        rm -rf build_ompt_nvhpc_${ARCH}
-    fi
-    mkdir build_ompt_nvhpc_${ARCH} && cd build_ompt_nvhpc_${ARCH}
-    cmake -D CMAKE_BUILD_TYPE=Release \
-	  -D CMAKE_INSTALL_PREFIX=${KOKKOS_PATH}/install_ompt_nvhpc \
-	  -D CMAKE_CXX_COMPILER=nvc++ \
-	  -D CMAKE_CXX_STANDARD=17 \
-	  -D Kokkos_ARCH_${ARCH}=ON \
-	  -D Kokkos_ENABLE_OPENMPTARGET=ON \
-	  -D Kokkos_ENABLE_IMPL_DESUL_ATOMICS=OFF \
-	  ..
-    make -j8
-    make install
-    cd ..
-
-    cd ..
+if [ ! -d build_cuda_nvhpc ]; then
+    echo "Cuda build of kokkos not found. Please run the Kokkos script first."
 fi
 
-if [ ! -d TestSNAP-Kokkos ]; then
+if [ ! -d build_cuda_ompt ]; then
+    echo "OpenMPTarget build of kokkos not found. Please run the Kokkos script first."
+fi
+cd ${HOME}
+
+if [ ! -d TestSNAP ]; then
     git clone --single-branch --branch Kokkos-nvhpc https://github.com/rgayatri23/TestSNAP.git TestSNAP-Kokkos
 fi
 
-cd TestSNAP-Kokkos
+cd TestSNAP
 
-    if [ -d build_cuda_nvhpc_${ARCH} ]; then
-        rm -rf build_cuda_nvhpc_${ARCH}
+    if [ -d build_cuda_nvhpc ]; then
+        rm -rf build_cuda_nvhpc
     fi
-    mkdir build_cuda_nvhpc_${ARCH} && cd build_cuda_nvhpc_${ARCH}
+    mkdir build_cuda_nvhpc && cd build_cuda_nvhpc
     cmake -D CMAKE_BUILD_TYPE=Release \
           -D CMAKE_CXX_COMPILER=${KOKKOS_PATH}/bin/nvcc_wrapper \
           -D CMAKE_C_COMPILER=$(which gcc) \
           -D CMAKE_CXX_STANDARD=17 \
           -D CMAKE_CXX_EXTENSIONS=OFF \
-          -D Kokkos_ROOT=../../kokkos/install_cuda_nvhpc \
+          -D Kokkos_ROOT=${KOKKOS_PATH}/install_cuda_nvhpc \
           -D ref_data=14 \
           ..
     make
     ${RUN} ./test_snap -ns 100
     cd ..
 
-    if [ -d build_ompt_nvhpc_${ARCH} ]; then
-        rm -rf build_ompt_nvhpc_${ARCH}
+    if [ -d build_ompt_nvhpc ]; then
+        rm -rf build_ompt_nvhpc
     fi
-    mkdir build_ompt_nvhpc_${ARCH} && cd build_ompt_nvhpc_${ARCH}
+    mkdir build_ompt_nvhpc && cd build_ompt_nvhpc
     cmake -D CMAKE_BUILD_TYPE=Release \
           -D CMAKE_CXX_COMPILER=nvc++ \
           -D CMAKE_C_COMPILER=nvc \
           -D CMAKE_CXX_STANDARD=17 \
           -D CMAKE_CXX_EXTENSIONS=OFF \
-          -D Kokkos_ROOT=../../kokkos/install_ompt_nvhpc \
+          -D Kokkos_ROOT=${KOKKOS_PATH}/install_ompt_nvhpc \
           -D ref_data=14 \
           ..
     make
